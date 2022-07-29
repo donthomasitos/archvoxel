644d69a0273d19ac2b5b69e8675e83c56dd0aabe
donthomasitos <github@thomaswillberger.com>
2022-07-29 14:04:43 +0200

    trying speedup in latent prep, but is slower

--- latent_preprocess.py
+++ latent_preprocess.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 from flax import jax_utils
 from datetime import datetime
 
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 DESIRED_SAMPLES = 5e6
 
 # This generation is theoretically suboptimal, as it can generate duplicates. BUT:
@@ -36,6 +36,7 @@ def main():
 
     rng = jax.random.PRNGKey(0)
     rng, key, z_rng = jax.random.split(rng, 3)
+    local_device_count = jax.local_device_count()
 
     def random_crop_mirror(random_int):
         data_id = random_int % len(model_datas)  # TODO sample proportionally to model size
@@ -55,15 +56,19 @@ def main():
         def _prepare(x):
             # Use _numpy() for zero-copy conversion between TF and NumPy.
             x = x._numpy()  # pylint: disable=protected-access
-            return x # .reshape((1, -1) + x.shape[1:])
+            # reshape (host_batch_size, height, width, 3) to
+            # (local_devices, device_batch_size, height, width, 3)
+            return x.reshape((local_device_count,) + x.shape[1:])
+
         return jax.tree_map(_prepare, xs)
 
     random.seed(datetime.now())
     dataset = tf.data.Dataset.random(random.randint(0, 100000))
-
     dataset = dataset.map(augmentation)
+    dataset = dataset.batch(local_device_count)
     dataset = dataset.prefetch(tf.data.AUTOTUNE).repeat()
     dataset = map(prepare_tf_data, dataset)
+    dataset = jax_utils.prefetch_to_device(dataset, 2)
 
     print("Loading VAE local model...")
     vae_state = utils.get_vae_state(resolution, vae_params, key, rng, "runs/" + config["local_run"], 0.01)
@@ -71,13 +76,27 @@ def main():
 
     @jax.jit
     def get_local_latent(images):
-        def eval_model(vae):
-            return vae.encoder(images)
+        def eval_model(vae, x):
+            return vae.encoder(x)
 
-        mean, logvar = nn.apply(eval_model, vae_model)({'params': vae_state.params})
-        return jnp.stack([mean, logvar], axis=1)  # xyzc are later merged into one latent variable. separate the data right after the batch dimension.
+        batch = jnp.reshape(images, (-1, *images.shape[-4:]))
+        latent_splits = batch.shape[0] // BATCH_SIZE
+        # print(f"Dividing {batch.shape[0]} into {latent_splits}. batch.shape: {batch.shape}")
+        assert batch.shape[0] % BATCH_SIZE == 0, "number of voxels must be dividable by batch size OR implement non-full remaining batch..."
+        batch = jnp.split(batch, latent_splits, axis=0)
+        apply_fn = nn.apply(eval_model, vae_model)
+        latents = []
+        for s in range(latent_splits):
+            split = batch[s]
+            #print("in_data", split.shape)
+            mean, logvar = apply_fn({'params': vae_state.params}, split)
+            #print("mean", mean.shape)
+            local_out = jnp.stack([mean, logvar], axis=1)  # xyzc are later merged into one latent variable. separate the data right after the batch dimension.
+            res = jnp.reshape(local_out, (BATCH_SIZE, 2 * lat_dim))
+            #print("res", res.shape)
+            latents.append(res)
+        return jnp.concatenate(latents, axis=0)
 
-    local_device_count = jax.local_device_count()
     assert BATCH_SIZE % local_device_count == 0, "Batch size must be divisable by device count for parallelization"
     get_local_latent_p = jax.pmap(get_local_latent)
     save_path = "/media/thomas/Data/latents/" + config["local_run"]
@@ -92,27 +111,12 @@ def main():
     with tqdm(initial=num_generated, total=DESIRED_SAMPLES) as pbar:
         while num_generated < DESIRED_SAMPLES:
             batch = next(dataset)
-            batch = jnp.reshape(batch, (-1, *batch.shape[-4:]))
-            latent_splits = batch.shape[0] // BATCH_SIZE
-            #print(f"Dividing {batch.shape[0]} into {latent_splits}. batch.shape: {batch.shape}")
-            assert batch.shape[0] % BATCH_SIZE == 0, "number of voxels must be dividable by batch size OR implement non-full remaining batch..."
-            batch = jnp.split(batch, latent_splits, axis=0)
-            latents = []
-            for split in batch:
-                in_data = jnp.reshape(split, (local_device_count, BATCH_SIZE // local_device_count, *split.shape[1:]))
-                #print("in_data", in_data.shape)
-                local_out = get_local_latent_p(in_data)
-                #print("local_out", local_out.shape)
-                res = jnp.reshape(local_out, (BATCH_SIZE, 2 * lat_dim))
-                #print("res", res.shape)
-                latents.append(res)
-
-            latents = jnp.concatenate(latents, axis=0)
-            latents = jnp.reshape(latents, (lat_size, lat_size, lat_size, 2 * lat_dim))
+            latents = get_local_latent_p(batch)
+            latents = jnp.reshape(latents, (local_device_count, lat_size, lat_size, lat_size, 2 * lat_dim))
 
             latens_write_buffer.append(latents)
             if len(latens_write_buffer) == min(DESIRED_SAMPLES, 100):
-                write_buf = jnp.stack(latens_write_buffer, axis=0)
+                write_buf = jnp.concatenate(latens_write_buffer, axis=0)
                 print("Saving... (Dont close)")
                 with h5py.File(save_path + '/latents_meanvar.hdf5', 'a') as out_file:
                     old_file_size = out_file["latents"].shape[0]
